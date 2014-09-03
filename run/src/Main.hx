7a03ca25499fbbf9d733c5bcb28d67e76b49ff0e
Yaroslav Sivakov <Yaroslav Sivakov@localhost>
2014-09-03 18:26:33 +0400

    * correct installing on Windows (haxescript.cmd is not need anymore)

--- run/src/Main.hx
+++ run/src/Main.hx
@@ -1,10 +1,12 @@
 import hant.CmdOptions;
+import hant.PathTools;
 import hant.Process;
+import haxe.io.Path;
 import neko.Lib;
+import neko.winapi.Registry;
+import Sys;
 import sys.FileSystem;
 import sys.io.File;
-import Sys;
-import hant.PathTools;
 using StringTools;
 
 class Main 
@@ -29,24 +31,50 @@ class Main
 		{
 			Lib.println("Using: haxelib run haxescript <script_file> [ <script_options>... ]");
 			Lib.println("");
-			if (Sys.systemName() != "Windows")
-			{
-				Lib.println("To install on *nix: haxelib run haxescript --install");
-				Lib.println("After install you could use '#!/usr/bin/haxescript' in your scripts.");
-			}
-			else
-			{
-				Lib.println("On Windows you can manually copy haxescript.cmd to folder in PATH.");
-				Lib.println("After this you could use 'haxescript myscript.hs' to run your script.");
-			}
+			Lib.println("To install run: haxelib run haxescript --install");
+			Lib.println("After this you could use '#!/usr/bin/haxescript' in your scripts (on *nix platforms).");
+			Lib.println("On Windows *.hs files will be associated with haxescript.");
+			Lib.println("Warning: on Windows you can't use more than 9 script options when run *.hs.");
 			Sys.exit(1);
 		}
 	}
 	
 	static function install()
 	{
-		File.saveContent("/usr/bin/haxescript", "#!/bin/sh\nhaxelib run haxescript $@\n");
-		Sys.exit(Sys.command("chmod", [ "a+x", "/usr/bin/haxescript" ]));
+		if (Sys.systemName() != "Windows")
+		{
+			File.saveContent("/usr/bin/haxescript", "#!/bin/sh\nhaxelib run haxescript $@\n");
+			Sys.exit(Sys.command("chmod", [ "a+x", "/usr/bin/haxescript" ]));
+		}
+		else
+		{
+			var haxelib = "";
+			
+			if (Sys.getEnv("HAXEPATH") != null)
+			{
+				haxelib = Path.addTrailingSlash(Sys.getEnv("HAXEPATH")) + "haxelib.exe";
+			}
+			
+			if (haxelib == "" || !FileSystem.exists(haxelib))
+			{
+				for (path in Sys.getEnv("PATH").split(";"))
+				{
+					haxelib = Path.addTrailingSlash(path) + "haxelib.exe";
+					if (FileSystem.exists(haxelib)) break;
+				}
+			}
+			
+			if (haxelib == "" || !FileSystem.exists(haxelib))
+			{
+				Lib.println("Error: haxelib is not found (set HAXEPATH environment variable or include folder with haxelib.exe in PATH ).");
+				Sys.exit(2);
+			}
+			
+			haxelib = haxelib.replace("/", "\\");
+			
+			Registry.setKeyValueString(Registry.HKEY_CLASSES_ROOT, "", ".hs", "HaxeScript");
+			Registry.setKeyValueString(Registry.HKEY_CLASSES_ROOT, "HaxeScript\\shell\\open", "command", "\"" + haxelib + "\" run haxescript \"%1\" \"%2\" \"%3\" \"%4\" \"%5\" \"%6\" \"%7\" \"%8\" \"%9\"");
+		}
 	}
 	
 	static function runScript(scriptFile:String)
@@ -77,6 +105,8 @@ class Main
 			interp.variables.set("Lambda", Lambda);
 			interp.variables.set("Process", Process);
 			interp.variables.set("CmdOptions", CmdOptions);
+			interp.variables.set("Path", Path);
+			interp.variables.set("PathTools", PathTools);
 			
 			var exitCode = Std.parseInt(interp.execute(program));
 			
